00e26b8b8ce370779061a671bee5e341771802d3
frankxzx <frankxzx@hotmail.com>
2015-06-02 15:54:53 +0800

    Initial Commit

---
+++ DemoOfSnapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoOfSnapKit
+//
+//  Created by Xzx 's iMac on 15/6/2.
+//  Copyright (c) 2015年 touch-spring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
